209564a4ca0c268238d7fa80ed8b621229fd9485
Michael Boguszewski <mboguszewski6039@stu.d214.org>
2023-04-14 13:02:41 -0500

    commit

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var integer1: Int
-    @State var integer2: Int
+    @State var integer1: Int = 0
+    @State var integer2: Int = 0
     var body: some View {
         TextField("Integer1", value: $integer1, format: .number)
         TextField("Integer2", value: $integer2, format: .number)
