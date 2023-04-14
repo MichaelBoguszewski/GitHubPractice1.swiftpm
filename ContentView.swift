f47fa618eb5cab5fd8f8fa11459cdf03d39be550
Nazar <nkochylo5186@stu.d214.org>
2023-04-14 12:54:53 -0500

    commit

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Number = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button {
+            
+        } label: {
+            Text("Multiply")
         }
+
     }
 }
