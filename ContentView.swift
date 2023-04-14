486fe035ea8c6f05bc182ea26a2254804256ce18
Nazar <nkochylo5186@stu.d214.org>
2023-04-14 13:06:36 -0500

    Added button

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var integer1: Int
-    @State var integer2: Int
+    @State var Number1 = 0
     var body: some View {
-        TextField("Integer1", value: $integer1, format: .number)
-        TextField("Integer2", value: $integer2, format: .number)
+        Button {
+            
+        } label: {
+            Text("Multiply")
+        }
+
     }
 }
